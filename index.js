872b2ae35e1e50b75fdf27677637abffb1df9433
Raymond Zhu <raymond.zhu@ansarada.com>
2016-10-16 21:22:17 +1100

    fix request

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var staticServer = require('node-static');
 var server = new staticServer.Server('./');
 
 require('http').createServer(function(req, resp) {
-  request.addListener('end', function() {
+  req.addListener('end', function() {
     server.serve(req, resp);
   })
 }).listen(process.env.PORT || 5000);
