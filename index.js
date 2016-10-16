092e273e54a3d594fe73114a25e05452c575d7fd
Raymond Zhu <raymond.zhu@ansarada.com>
2016-10-16 21:48:10 +1100

    add debug log

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var staticServer = require('node-static');
 
 var server = new staticServer.Server('./');
-
+var path = require('path');
+console.log(path.resolve('./'));
+console.log(__dirname);
 require('http').createServer(function(req, resp) {
   req.addListener('end', function() {
     server.serve(req, resp);
+
   })
 }).listen(process.env.PORT || 5000);
